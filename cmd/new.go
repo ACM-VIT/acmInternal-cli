33b7630968036ff5e449e378aa1a6e72a48b10e6
madrigal1 <kurupgokul11@gmail.com>
2021-01-24 17:43:59 +0530

    feat: new version 2.0

--- cmd/new.go
+++ cmd/new.go
@@ -68,5 +68,5 @@ func init() {
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// newCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	//newCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
